73ab9ed1ed851e5dc04058cda495daab4f12fc64
eshom <erez@mandor>
2021-03-26 18:13:44 +0300

    Cleanup superflous whitespace

--- R/generate_questions.R
+++ R/generate_questions.R
@@ -19,17 +19,17 @@ sanitize_input <- function(x){
   } else {
     x$mandatory <- NULL
   }
-  
+
   if( !is.null(x[["choices"]]) ){
     x$choices <- scan(text = x$choices, what = character(), sep = ",",
                       strip.white = TRUE, quiet = TRUE)
     x$choices <- x$choices[x$choices != ""]
-    
+
     if( is.na(x$selected) ){
       x$selected <- character(0)
     }
   }
-  
+
   if( is.na(x$width) ){
     x$width <- NULL
   }
@@ -44,8 +44,8 @@ select_cols <- function(df, ..., prefix=NULL){
   } else {
     df <- select(df, ...)
   }
-  
-  df %>% 
+
+  df %>%
     as.list() %>%
     sanitize_input()
 }
@@ -78,18 +78,18 @@ choose_input <- function(input_df) {
 # Create a div in shiny UI with input objects corresponding to questions in the googlesheet
 populate_questions <- function(ss, sheet, div_id){
   questions <- read_sheet(ss = ss, sheet = sheet)
-  
+
   inputs <- questions %>%
     rowwise() %>%
     group_map(~choose_input(.x))
-  
+
   return(eval(bquote(htmltools::div(
-    id = div_id, 
+    id = div_id,
     ..(inputs),
     actionButton(
-      inputId = "submit", 
-      label = "Submit", 
-      class = "btn-primary", 
+      inputId = "submit",
+      label = "Submit",
+      class = "btn-primary",
       width = 350
     )
   ), splice = TRUE)))
@@ -100,5 +100,3 @@ populate_questions <- function(ss, sheet, div_id){
 #   sheet = "Questions",
 #   div_id = "form"
 # )
-
-
